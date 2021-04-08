275ee45150fd15790aca862ee60005da26475272
Dion Alting <dion_a@live.nl>
2021-04-08 12:15:32 +0200

    Basic login form setup

---
+++ src/components/login/LoginForm/component.tsx
@@ -0,0 +1,97 @@
+import { Link } from "react-router-dom";
+
+export const LoginForm = () => {
+  return (
+    <div className="mt-8">
+      <form action="#" autoComplete="off">
+        <div className="flex flex-col mb-2">
+          <div className="flex relative ">
+            <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                />
+              </svg>
+            </span>
+            <input
+              type="text"
+              className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+              placeholder="Your email"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col mb-6">
+          <div className="flex relative ">
+            <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                />
+              </svg>
+            </span>
+            <span className="absolute inset-y-0 right-1 flex items-center pl-2">
+              <button
+                onClick={() => console.log("clicked show password button")}
+                className="p-1 focus:outline-none focus:shadow-outline"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 text-gray-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                  />
+                </svg>
+              </button>
+            </span>
+            <input
+              type="password"
+              className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+              placeholder="Your password"
+            />
+          </div>
+        </div>
+        <div className="flex w-full">
+          <button
+            type="submit"
+            className="py-2 px-4  bg-green-400 hover:bg-green-600 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+          >
+            Login
+          </button>
+        </div>
+      </form>
+      <div className="flex items-center justify-center mt-6">
+        <Link
+          to="/signup"
+          className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white"
+        >
+          <span className="ml-2">You don&#x27;t have an account?</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/login/LoginForm/container.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { LoginForm } from "./component";
+const loginFormContainer = () => {
+  return <LoginForm />;
+};
+export default loginFormContainer;
