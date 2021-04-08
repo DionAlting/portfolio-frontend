4b22d038fcb7c4dc7c80bd84b93c26a557277955
Dion Alting <dion_a@live.nl>
2021-04-08 12:16:35 +0200

    Created login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoginUI } from "../components/login";
+
+const Login = () => {
+  return (
+    <div className="container mx-auto px-4 h-full flex justify-center items-center font-body ">
+      <LoginUI />
+    </div>
+  );
+};
+
+export default Login;
