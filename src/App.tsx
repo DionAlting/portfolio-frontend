488007e395d639ed7c924c32ff8ac06ffc475000
Dion Alting <dion_a@live.nl>
2021-04-08 12:16:08 +0200

    Added switch and login form to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+
+import { Route, Switch } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <Navbar />{" "}
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
