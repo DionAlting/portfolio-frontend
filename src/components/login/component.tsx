2e2f936f0ec46e7fd05402681784658ad8f447f7
Dion Alting <dion_a@live.nl>
2021-04-08 13:57:37 +0200

    Added logo to login

--- src/components/login/component.tsx
+++ src/components/login/component.tsx
@@ -1,8 +1,14 @@
 import { LoginForm } from "./LoginForm";
+import Logo from "../../assets/Logo_no_margin.svg";
 
 export const Login = () => {
   return (
     <div className="flex flex-col w-full max-w-md mt-10 px-4 py-8 bg-white border-t-2 border-green-400 rounded-lg shadow-xl sm:px-6 md:px-8 lg:px-10">
+      <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl">
+        <div className="bg-green-400 rounded-full p-2">
+          <img src={Logo} alt="HSG Logo" className="h-14 w-14" />
+        </div>
+      </div>
       <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl">
         Login To Your Account
       </div>
