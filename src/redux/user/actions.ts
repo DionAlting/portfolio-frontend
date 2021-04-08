3ee65a6022464429a900df41d29f23402da0148f
Dion Alting <dion_a@live.nl>
2021-04-08 20:50:47 +0200

    Removed old console logs from user actions

--- src/redux/user/actions.ts
+++ src/redux/user/actions.ts
@@ -18,7 +18,6 @@ const getUserProfile = async (token: string) => {
         Authorization: `Bearer ${token}`,
       },
     });
-    // console.log(response);
     return response.data;
   } catch (e) {
     console.log(e);
@@ -33,25 +32,20 @@ export const login = (email: string, password: string) => async (
       email,
       password,
     });
-    // console.log(response);
     const { token } = response.data;
 
     const userProfile = await getUserProfile(token);
 
     localStorage.setItem("jwt", token);
-    console.log(response);
     dispatch(saveUserData(token, userProfile));
   } catch (e) {
     console.log("error", e);
   }
-  // do POST request to /login,
-  // console.log response
 };
 
 export const bootstrapLogin = () => async (dispatch: Dispatch) => {
   const jwt = localStorage.getItem("jwt");
   if (jwt) {
-    // make /me call
     const userProfile = await getUserProfile(jwt);
     console.log("user profile loaded automatically", userProfile);
     dispatch(saveUserData(jwt, userProfile));
