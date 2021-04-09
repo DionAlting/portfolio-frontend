e1317a5311bd00d155c5c092b6f591cbaf3256f1
Dion Alting <dion_a@live.nl>
2021-04-09 10:48:45 +0200

    Added SignUp to switch and Initial page setup

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="container mx-auto px-4 h-full flex justify-center items-center font-body ">
+      <h1>Hi</h1>
+    </div>
+  );
+};
+
+export default SignUp;
