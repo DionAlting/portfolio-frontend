d9773530b13b7511ed92e63851bb4a4a5268f2bb
Dion Alting <dion_a@live.nl>
2021-04-09 14:49:47 +0200

    Added showPassword,types and handleSubmit

--- src/components/SignUp/SignUpForm/container.tsx
+++ src/components/SignUp/SignUpForm/container.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { SignUpForm } from "./component";
+import { submitValues } from "./types";
 
 const SignUpFormContainer = () => {
-  return <SignUpForm />;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  const handleSubmit = (values: submitValues) => {
+    const { displayName, email, firstName, lastName, password } = values;
+  };
+  return (
+    <SignUpForm
+      showPassword={showPassword}
+      handleClickShowPassword={handleClickShowPassword}
+      handleSubmit={handleSubmit}
+    />
+  );
 };
 export default SignUpFormContainer;
