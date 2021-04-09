040da0d16c6f3451562658ae092d44f2700ce7f6
Dion Alting <dion_a@live.nl>
2021-04-09 16:26:50 +0200

    Initial HomePage setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { Navbar } from "./components/Navbar";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="relative min-h-screen bg-gray-100 pb-40 md:pb-10">
+    <div className="relative min-h-screen pb-40 bg-gray-100 md:pb-10">
       <Navbar />
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/" component={Home} />
       </Switch>
     </div>
   );
