5c38c703d721789a83b39a89885b6458553c91a2
Dion Alting <dion_a@live.nl>
2021-04-12 11:36:45 +0200

    Created ReserveForm types

---
+++ src/components/Reserve/ReserveForm/types.ts
@@ -0,0 +1,18 @@
+export type ReserveFormProps = {
+  allDates: Date[];
+  handleDateChange: (id: string) => void;
+  singleDate: Date;
+  isAuthenticated: boolean;
+  firstName: string;
+  lastName: string;
+};
+
+export type Date = {
+  id: string;
+  date: string;
+  maxSeats: number;
+  maxPerParty: number;
+  bookedSeats: number;
+  isStampable: boolean;
+  isActive: boolean;
+};
