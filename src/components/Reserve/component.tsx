6902d88c5fb6d196c7de5b2c804bc3b1fd4a40ee
Dion Alting <dion_a@live.nl>
2021-04-12 11:36:29 +0200

    Added Form and info card to Reserve page

--- src/components/Reserve/component.tsx
+++ src/components/Reserve/component.tsx
@@ -1,26 +1,15 @@
 import React from "react";
+import { AvailableSeatsCard } from "../AvailableSeatsCard/";
+import { ReserveForm } from "./ReserveForm/";
 
 export const Reserve = () => {
   return (
-    <div className="relative w-full p-4 overflow-hidden text-gray-700 bg-white border-t-2 border-green-400 shadow-lg rounded-xl dark:bg-gray-800 dark:text-gray-100">
-      <div className="w-full">
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
-        <h1>ReserveForm</h1>
+    <div className="container flex flex-wrap items-center px-5 py-12 mx-auto md:py-24 font-body">
+      <div className="w-full px-5 bg-white border-t-2 border-green-400 shadow-lg md:h-50 lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 rounded-xl">
+        <ReserveForm />
+      </div>
+      <div className="flex flex-col w-full mt-10 rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
+        <AvailableSeatsCard />
       </div>
     </div>
   );
