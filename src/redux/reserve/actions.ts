31ecf9a18f5f0745660a4c288c701775a3ed0af9
Dion Alting <dion_a@live.nl>
2021-04-12 13:26:10 +0200

    Added new reservation action

--- src/redux/reserve/actions.ts
+++ src/redux/reserve/actions.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Dispatch } from "redux";
 import { FETCH_DATES_SUCCESS } from "../actionTypes";
+import { ReduxState } from "../store";
 import { ReservationDatePayload } from "./types";
 
 const API_URL = process.env.REACT_APP_API_URL;
@@ -14,4 +15,25 @@ export const fetchDates = () => async (dispatch: Dispatch) => {
   const dateResponse = await axios.get(`${API_URL}/reserve/dates`);
 
   dispatch(datesFetchedSuccess(dateResponse.data));
+};
+
+export const submitNewReservation = (values: any) => async (
+  dispatch: Dispatch,
+  getState: () => ReduxState
+) => {
+  try {
+    const { accessToken } = getState().user;
+    const { dateId, reservationDetails, comment } = values;
+    console.log(accessToken, values);
+    const response = await axios.post(
+      `${API_URL}/reserve/${values.dateId}`,
+      { dateId, reservationDetails, comment },
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+    console.log(response);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
