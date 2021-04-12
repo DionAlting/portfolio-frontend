6e5b7701d867b3b443b3fb4cd5c311244db1106a
Dion Alting <dion_a@live.nl>
2021-04-12 11:37:20 +0200

    Created new singleDate selector

--- src/redux/reservationDates/selectors.ts
+++ src/redux/reservationDates/selectors.ts
@@ -1,3 +1,11 @@
 import { ReduxState } from "../store";
 
 export const reservationDates = (state: ReduxState) => state.reservationDates;
+
+export const singleDate = (id: string) => (state: ReduxState) => {
+  const clonedDates = [...state.reservationDates];
+  const specificDay = clonedDates.find((date) => {
+    return id === date.id;
+  });
+  return specificDay;
+};
