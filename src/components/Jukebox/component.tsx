f6f504ad55b034abc184c90c18bf520bc2c88f96
Dion Alting <dion_a@live.nl>
2021-04-12 14:15:59 +0200

    Created JukeboxUI component

---
+++ src/components/Jukebox/component.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Jukebox = () => {
+  return (
+    <div className="container flex flex-col w-full mx-auto">
+      <div className="px-4 py-5 mt-4 mb-2 bg-white border-t-2 border-green-400 rounded-md shadow sm:px-6">
+        <h3 className="text-lg font-medium leading-6 text-gray-900 dark:text-white">
+          Jukebox
+        </h3>
+        <p className="max-w-2xl mt-1 text-sm text-gray-500 ">
+          Request a song or vote on other requested songs!
+        </p>
+      </div>
+      TODO: Requested songs list
+    </div>
+  );
+};
--- src/pages/Jukebox.tsx
+++ src/pages/Jukebox.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { JukeboxUI } from "../components/Jukebox";
 
 const Jukebox = () => {
   return (
-    <div>
-      <h1>Jukebox</h1>
+    <div className="container p-10 px-6 mx-auto font-body ">
+      <JukeboxUI />
     </div>
   );
 };
