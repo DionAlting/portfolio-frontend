2b773ed8767e87e6bc0c6087592f932c3e69f328
Dion Alting <dion_a@live.nl>
2021-04-14 10:18:40 +0200

    Created basic profile layout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { ProfileUI } from "../components/Profile";
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="container p-10 px-6 mx-auto font-body ">
+      <ProfileUI />
     </div>
   );
 };
