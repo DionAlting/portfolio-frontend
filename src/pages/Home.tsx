ed1295559986ed2ceef7f1ddc17246faa40f705c
Dion Alting <dion_a@live.nl>
2021-04-15 12:46:52 +0200

    Fixed some styling on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,19 +8,19 @@ const Home = () => {
     <>
       <div className="container flex flex-wrap items-center px-5 py-10 mx-auto bg-white rounded-lg lg:my-10 lg:py-24 font-body">
         <div className="pr-0 lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0">
-          <h1 className="text-3xl font-extrabold text-gray-900 title-font">
+          <h1 className="text-2xl font-extrabold text-gray-900 title-font lg:text-4xl">
             Haerlems Studenten Gildt online reservation and stamp cards
           </h1>
-          <p className="mt-4 font-bold leading-relaxed text-green-400">
+          <p className="mt-4 font-bold leading-relaxed text-green-400 lg:text-2xl">
             Do you want to claim a spot in your favorite bar?
           </p>
-          <div className="mt-5">
-            <button
+          <div className="mt-10">
+            <Link
               className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-green-500 rounded shadow outline-none active:bg-emerald-600 hover:shadow-lg focus:outline-none"
-              type="button"
+              to="/reserve"
             >
-              <Link to="/reserve">Reserve now</Link>
-            </button>
+              Reserve now!
+            </Link>
           </div>
         </div>
         <div className="flex flex-col w-full mt-10 rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
