d72e23cd0bd00b30d5b1a4489af6d2aadc235bbb
Dion Alting <dion_a@live.nl>
2021-04-15 12:36:45 +0200

    Finished events component

--- src/components/Events/component.tsx
+++ src/components/Events/component.tsx
@@ -1,48 +1,70 @@
 import React from "react";
+import moment from "moment";
 
-export const Events = () => {
+import { EventProps } from "./types";
+
+import stampImage from "../../assets/stamp.png";
+import noStampImage from "../../assets/no_stamp.png";
+import { CheckCircleIcon } from "@heroicons/react/outline";
+
+export const Events = (props: EventProps) => {
   return (
-    <div className="container w-full p-6 bg-white lg:p-12">
+    <div className="container w-full p-6 mx-auto bg-green-300 rounded-t-lg lg:p-12 font-body">
       <div className="flex items-end justify-between mb-12 header">
         <div className="title">
-          <p className="mb-4 text-4xl font-bold text-gray-700">
-            Upcoming events
-          </p>
+          <p className="mb-4 text-4xl font-bold text-white">Upcoming events</p>
         </div>
       </div>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-1 xl:grid-cols-1">
-        <div className="m-auto overflow-hidden rounded-lg shadow-lg lg:w-3/5">
-          <img
-            alt="event"
-            src="https://via.placeholder.com/1024x512"
-            className="object-cover w-full max-h-80"
-          />
-          <div className="w-full p-4 bg-white dark:bg-gray-800">
-            <p className="font-medium text-green-500 text-md">hsg</p>
-            <p className="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-              Event
-            </p>
-            <p className="font-light text-gray-400 dark:text-gray-300 text-md">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-              nobis eaque maxime error quia sit rerum cupiditate architecto,
-              nulla laborum fugiat ex vel iste impedit nemo ullam delectus rem
-              corporis!
-            </p>
-            <div className="flex items-center mt-4">
-              <div className="flex flex-col justify-between flex-grow ml-4 text-sm">
-                <p className="text-gray-800 dark:text-white">HSG</p>
-                <p className="text-gray-400 dark:text-gray-300">
-                  event date - event time
-                </p>
-              </div>
+        {props.events &&
+          props.events.map((event) => (
+            <div
+              className="m-auto overflow-hidden rounded-lg shadow-lg lg:w-3/5"
+              key={event.id}
+            >
               <img
-                alt="stamp"
-                src="/images/person/6.jpg"
-                className="object-cover w-10 h-10 rounded-full "
+                alt="event"
+                src={event.eventImage}
+                className="object-cover w-full max-h-80"
               />
+              <div className="w-full p-4 bg-white dark:bg-gray-800">
+                <span
+                  className={`font-medium px-4 py-2 rounded-full text-white bg-${event.creator.studyAssociation.color}-500 text-xs`}
+                >
+                  {event.creator.studyAssociation.name}
+                </span>
+                <div className="flex flex-row items-center justify-between py-2 mr-1">
+                  <p className="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                    {event.title}
+                  </p>
+                  <img
+                    alt="stamp"
+                    src={event.isStampable ? stampImage : noStampImage}
+                    className="object-cover w-10 h-10 ml-2 rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col justify-between flex-grow mb-5 text-sm">
+                  <p className="text-gray-800 dark:text-white">
+                    {moment(event.eventDate).fromNow()}
+                  </p>
+                  <p className="text-gray-400 dark:text-gray-300">
+                    From {moment(event.startTime).format("LT")} till{" "}
+                    {moment(event.endTime).format("LT")}
+                  </p>
+                </div>
+
+                <p className="font-light text-gray-400 dark:text-gray-300 text-md">
+                  {event.content}
+                </p>
+                <div className="flex items-center mt-4">
+                  <p className="flex items-center text-sm text-gray-300 flex-2">
+                    <CheckCircleIcon className="w-6 h-6 mr-1 text-green-500" />{" "}
+                    {event.attendees.length} Attendees
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          ))}
       </div>
     </div>
   );
--- src/components/Events/container.tsx
+++ src/components/Events/container.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Event } from "../../redux";
 
 import { Events } from "./component";
 
 const EventsContainer = () => {
-  return <Events />;
+  const dispatch = useDispatch();
+  const events = useSelector(Event.Selectors.events);
+
+  useEffect(() => {
+    dispatch(Event.Actions.fetchEvents());
+  }, [dispatch]);
+
+  return <Events events={events} />;
 };
 
 export default EventsContainer;
