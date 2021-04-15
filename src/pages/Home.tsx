064d44c5d151bfccb463dff4bbb6379b296247bf
Dion Alting <dion_a@live.nl>
2021-04-15 12:36:23 +0200

    Fixed some styling bugs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,14 +6,13 @@ import { Events } from "../components/Events";
 const Home = () => {
   return (
     <>
-      <div className="container flex flex-wrap items-center px-5 py-10 mx-auto lg:py-24 font-body">
+      <div className="container flex flex-wrap items-center px-5 py-10 mx-auto bg-white rounded-lg lg:my-10 lg:py-24 font-body">
         <div className="pr-0 lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0">
-          <h1 className="text-3xl font-medium text-gray-900 title-font">
-            Slow-carb next level shoindcgoitch ethical authentic, poko scenester
+          <h1 className="text-3xl font-extrabold text-gray-900 title-font">
+            Haerlems Studenten Gildt online reservation and stamp cards
           </h1>
-          <p className="mt-4 leading-relaxed">
-            Poke slow-carb mixtape knausgaard, typewriter street art gentrify
-            hammock starladder roathse. Craies vegan tousled etsy austin.
+          <p className="mt-4 font-bold leading-relaxed text-green-400">
+            Do you want to claim a spot in your favorite bar?
           </p>
           <div className="mt-5">
             <button
