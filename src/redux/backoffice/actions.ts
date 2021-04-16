591d97dd1957aed8a70d9a883a766dd8300fe3ae
Dion Alting <dion_a@live.nl>
2021-04-16 09:43:20 +0200

    Added toats to backoffice actions

--- src/redux/backoffice/actions.ts
+++ src/redux/backoffice/actions.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import { Dispatch } from "redux";
 import { FETCH_RESERVATION_BY_DATE_SUCCESS } from "../actionTypes";
 import { ReduxState } from "../store";
@@ -28,6 +29,11 @@ export const fetchAllReservations = () => async (
     console.log(datesResponse.data);
     dispatch(fetchReservationsSuccess(datesResponse.data));
   } catch (error) {
+    if (error.response) {
+      toast.error(error.response.data.message);
+    } else {
+      toast.error("Something unexpected happened");
+    }
     console.log(error);
   }
 };
